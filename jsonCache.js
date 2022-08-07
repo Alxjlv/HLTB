1e7d8f748fc4c40b22ece75ddf78acb9130b3ccd
Alex Verkerk <44216323+Alxjlv@users.noreply.github.com>
2022-08-07 23:41:59 +1200

    📝 Added cache access to main function
    Also tightened up async/await to ensure everything exectues properly (eg. cache file write before access in load)
    
    Added random sleeping to debounce hitting the HLTB library

--- jsonCache.js
+++ jsonCache.js
@@ -1,46 +1,91 @@
 const fs = require("fs");
 const { parse } = require("csv-parse");
 
+const cacheFile = "gamesCache.json"
+
 function createCache(override) {
     try {
-        // found from https://stackabuse.com/reading-and-writing-json-files-with-node-js/
-        let currentCacheRaw = fs.readFileSync('gamesCache.json').toString()
-        let currentCache = JSON.parse(currentCacheRaw)
+        getCache();
+        if(!override){
+            return;
+        }
+    } catch (e) {
+        if (e instanceof CacheError) {
+            console.log(e.message);
+        } else {
+            throw e;
+        }
+    }
 
-        if (currentCache.games.length > 0 && !override) {
-            console.log("No override & cache already exists")
-            return
+    // load in list of games
+    // found from https://www.digitalocean.com/community/tutorials/how-to-read-and-write-csv-files-in-node-js-using-node-csv
+    // also using https://stackoverflow.com/a/58431762 for waiting on file writing
+    return new Promise(function (resolve, reject){
+        console.log("Creating cache");
+        let json = {
+            "games": []
+        }
+        fs.createReadStream("games.csv")
+            .pipe(parse({ delimiter: "\t", from_line: 2}))
+            .on("data", function (row) {
+                json.games.push({
+                    "name": row[0],
+                    "currentHours": row[1],
+                    "platform": row[2]
+                });
+            }).on("end", function (){
+                console.log("finished parsing csv");
+
+                // save json file to disk
+                saveCache(json);
+                resolve()
+            }).on('error', reject)
+    })
+}
+
+function getCache() {
+    let errorString = "";
+    let cache;
+    try {
+        // found from https://stackabuse.com/reading-and-writing-json-files-with-node-js/
+        let currentCacheRaw = fs.readFileSync(cacheFile).toString();
+        let currentCache = JSON.parse(currentCacheRaw);
+
+        if (currentCache.games.length === 0) {
+            errorString = "Cache is empty";
+        } else {
+            cache = currentCache;
         }
     } catch (e) {
         if(e.toString().includes("no such file or directory, open 'gamesCache.json'")) {
-            console.log("Cache did not exist, creating")
+            errorString = "Cache does not exist";
         } else {
-            console.log(e)
-            console.log("Cache errored")
+            console.log("Cache access errored");
+            throw e;
         }
     }
-
-    let json = {
-        "games": []
+    if (errorString !== "") {
+        throw new CacheError(errorString);
     }
-    console.log("Creating cache")
-    // load in list of games
-    // found from https://www.digitalocean.com/community/tutorials/how-to-read-and-write-csv-files-in-node-js-using-node-csv
-    fs.createReadStream("games.csv")
-        .pipe(parse({ delimiter: "\t", from_line: 2}))
-        .on("data", function (row) {
-            json.games.push({
-                "name": row[0],
-                "current hours": row[1],
-                "platform": row[2]
-            })
-        }).on("end", function (){
-            console.log("finished parsing csv")
-
-            // save json file to disk
-            let jsonString = JSON.stringify(json, null, 2)
-            fs.writeFileSync('gamesCache.json', jsonString)
-        })
+    return cache;
 }
 
-module.exports = createCache
+function saveCache(updatedCache) {
+    let jsonString = JSON.stringify(updatedCache, null, 2);
+    fs.writeFileSync(cacheFile, jsonString);
+    console.log("Cache saved to disk")
+}
+
+class CacheError extends Error {
+    constructor(message) {
+        super(message);
+        this.name = "CacheError";
+    }
+}
+
+
+module.exports = {
+    createCache,
+    getCache,
+    saveCache
+}
--- loadGames.js
+++ loadGames.js
@@ -1,7 +1,49 @@
-let hltb = require('howlongtobeat');
-const createCache = require("./jsonCache");
-let hltbService = new hltb.HowLongToBeatService();
+const hltb = require('howlongtobeat');
+const jsonCache = require("./jsonCache");
+const hltbService = new hltb.HowLongToBeatService();
 
-createCache(false)
-// will need to debounce the requests so that I don't get banned for DDoSing lol
-hltbService.search("Guardians of the Galaxy").then(result => console.log(result));
+const debugLimit = 1;
+const sleepMax = 5;
+
+async function main() {
+    await jsonCache.createCache(true);
+
+    let cache = jsonCache.getCache();
+
+    for (let i =0; i < cache.games.length; i++) {
+        let game = cache.games[i];
+        if (game.main && game)
+        console.log(game);
+        let hltbData = await debouncedRequest(game.name, sleepMax);
+        console.log(hltbData);
+        game["main"] = hltbData[0].gameplayMain
+        game["main&extra"] = hltbData[0].gameplayMainExtra
+        game["completionist"] = hltbData[0].gameplayCompletionist
+        game["hltbSimilarity"] = hltbData[0].similarity
+        game["hltbName"] = hltbData[0].name
+        console.log(cache.games[i])
+
+        // limit how many times the function is run for debugging purposes
+        if (i === (debugLimit - 1)){
+            break;
+        }
+    }
+    // jsonCache.saveCache(cache)
+}
+
+// debouncing calls to the service so that I don't get IP blocked for DDoSing lol
+async function debouncedRequest(searchQuery, maxSleepSeconds) {
+    let randomSleep = Math.floor(Math.random() * (maxSleepSeconds + 1)) * 1000;
+    console.log("Sleeping for " + randomSleep + " milliseconds");
+    await sleep(randomSleep);
+    console.log("Searching for " + searchQuery);
+    return await hltbService.search(searchQuery);
+}
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+main()
+
+
